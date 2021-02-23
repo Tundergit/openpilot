f5a2a51e7dc3300f845c42ab3de4b09771027215
acbgf8 <acbgf8@mail.missouri.edu>
2021-02-23 07:43:13 -0600

    Update carstate.py

--- selfdrive/car/chrysler/carstate.py
+++ selfdrive/car/chrysler/carstate.py
@@ -48,7 +48,7 @@ class CarState(CarStateBase):
 
     ret.cruiseState.enabled = cp.vl["FORWARD_CAMERA_ACC"]['ACC_STATUS'] == 3  # ACC is green.
     ret.cruiseState.available = ret.cruiseState.enabled  # FIXME: for now same as enabled
-   # ret.cruiseState.speed = cp.vl["DASHBOARD"]['ACC_SPEED_CONFIG_KPH'] * CV.KPH_TO_MS
+    ret.cruiseState.speed = cp.vl["FORWARD_CAMERA_CLUSTER"]['ACC_SPEED_SET_SPEED'] * CV.KPH_TO_MS
     # CRUISE_STATE is a three bit msg, 0 is off, 1 and 2 are Non-ACC mode, 3 and 4 are ACC mode, find if there are other states too
    # ret.cruiseState.nonAdaptive = cp.vl["DASHBOARD"]['CRUISE_STATE'] in [1, 2]
 
